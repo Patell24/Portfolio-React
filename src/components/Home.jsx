6759a5bbb2f6e307c9f6e6cd2607def433462666
Patell24 <realmeuhd4ktv@gmail.com>
2024-12-13 16:46:53 +0000

    social media

---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+
+function Home() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Feed</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1>Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
+              {/* <ReactTyped
+                className="text-red-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              /> */}
+            </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Quisquam, recusandae consequatur. Accusamus sint libero eligendi
+              laborum fuga repudiandae? Asperiores tempore eos animi dolores
+              corrupti! Sint quos, mollitia architecto aliquam nesciunt, optio
+              cum delectus sit voluptatum aut ratione quaerat veniam aperiam!
+            </p>
+            <br />
+            {/* Social Links */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="space-y-2">
+                <h1 className="font-bold text-center">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="">
+                    <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
